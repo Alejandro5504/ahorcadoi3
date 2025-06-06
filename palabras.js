b97fbd6e62c855b5efaa0b45c4cbdc81035c2455
Alejandro5504 <siseo15@hotmail.com>
2025-06-06 09:50:51 +0200

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,10 @@
+const LETRAS = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+// Recordar que los string son "como un array inmutable"; 
+const palabras = [
+    'CASA', 'AZUL', 'ROJO', 'AMARILLO', 'PALA',
+
+]
+const NUMERO_PALABRAS = palabras.length;
+console.log('Tenemos', NUMERO_PALABRAS, 'para jugar')
+
+const imagenes = ["cero","uno","dos","tres","cuatro","cinco","seis"];
